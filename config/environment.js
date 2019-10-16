993459030a2ef15c230e9be4daa356e5283d84e8
harrybabu <hviswanathan@linkedin.com>
2019-10-16 14:49:46 +0530

    Initial template commit

---
+++ config/environment.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = function(environment) {
+  let ENV = {
+    modulePrefix: 'game-2048',
+    environment
+  };
+
+  return ENV;
+};
---
+++ src/ui/components/Game2048/component.ts
@@ -0,0 +1,5 @@
+import Component from '@glimmer/component';
+
+export default class Game2048 extends Component {
+
+}
